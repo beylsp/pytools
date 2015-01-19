8ac1392b8d72814aa7691e4f49cb2bf334af54ff
Patrik Beyls <beylsp@yahoo>
2015-01-19 23:32:56 +0100

    add module metadata

--- pytools/__init__.py
+++ pytools/__init__.py
@@ -1,1 +1,10 @@
-__version__ = "0.1"
+"""various useful python tools."""
+
+__author__ = 'Patrik Beyls'
+__url__ = 'https://github.com/beylsp/pytools'
+__version__ = "0.0.1"
+
+import dump
+import wget
+import cli
+
