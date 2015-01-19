25c29f53cce64093841969104a8d558dacd3be8f
Patrik Beyls <beylsp@yahoo>
2015-01-19 23:03:10 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from pytools.__init__ import __version__
+from setuptools import setup, find_packages
+
+setup(
+    name="pytools",
+    version=__version__,
+    packages=find_packages(),
+)
